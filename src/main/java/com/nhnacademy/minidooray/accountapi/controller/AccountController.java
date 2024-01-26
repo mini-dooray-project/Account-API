c0fe80af56fb1274b7522ccf37dc40c9fb74cfa4
th915 <97163658+th915@users.noreply.github.com>
2024-01-27 00:54:45 +0900

    test commit

--- src/main/java/com/nhnacademy/minidooray/accountapi/controller/AccountController.java
+++ src/main/java/com/nhnacademy/minidooray/accountapi/controller/AccountController.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.minidooray.accountapi.controller;
 
 import com.nhnacademy.minidooray.accountapi.domain.AccountDto;
-import com.nhnacademy.minidooray.accountapi.entity.Account;
 import com.nhnacademy.minidooray.accountapi.exception.ValidationFailedException;
 import com.nhnacademy.minidooray.accountapi.model.AccountLoginRequest;
 import com.nhnacademy.minidooray.accountapi.model.AccountModifyRequest;
--- src/test/java/com/nhnacademy/minidooray/accountapi/controller/AccountControllerTest.java
+++ src/test/java/com/nhnacademy/minidooray/accountapi/controller/AccountControllerTest.java
@@ -1,12 +1,18 @@
 package com.nhnacademy.minidooray.accountapi.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.minidooray.accountapi.domain.AccountDto;
 import com.nhnacademy.minidooray.accountapi.entity.Account;
+import com.nhnacademy.minidooray.accountapi.model.AccountLoginRequest;
+import com.nhnacademy.minidooray.accountapi.model.AccountModifyRequest;
+import com.nhnacademy.minidooray.accountapi.model.AccountRegisterRequest;
 import com.nhnacademy.minidooray.accountapi.repository.AccountRepository;
 import java.time.LocalDate;
 import javax.transaction.Transactional;
@@ -28,6 +34,9 @@ class AccountControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     AccountRepository accountRepository;
 
     @BeforeEach
@@ -45,10 +54,31 @@ class AccountControllerTest {
     @Test
     @Transactional
     void testMatchResponse() throws Exception {
+        AccountLoginRequest accountRequest = new AccountLoginRequest("user", "1234");
+        mockMvc.perform(post("/api/accounts/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(accountRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.result").value("회원"));
     }
 
     @Test
-    void getAcount() throws Exception {
+    @Transactional
+    void testMatchResponseException() throws Exception {
+        AccountLoginRequest accountRequest = new AccountLoginRequest(null, "1234567");
+        mockMvc.perform(post("/api/accounts/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(accountRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value(
+                        "ObjectName=accountLoginRequest,Message=must not be blank,code=NotBlank"));
+    }
+
+    @Test
+    @Transactional
+    void testGetAccounts() throws Exception {
         mockMvc.perform(get("/api/accounts"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -56,14 +86,97 @@ class AccountControllerTest {
     }
 
     @Test
-    void createAccount() {
+    @Transactional
+    void testGetAccountsException() throws Exception {
+        accountRepository.deleteById("user");
+        mockMvc.perform(get("/api/accounts"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("accounts not found"));
     }
 
     @Test
-    void modifyAccount() {
+    @Transactional
+    void testGetAccount() throws Exception {
+        mockMvc.perform(get("/api/accounts/{id}", "user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", equalTo("유저")));
     }
 
     @Test
-    void deleteResponse() {
+    @Transactional
+    void testGetAccountException() throws Exception {
+        mockMvc.perform(get("/api/accounts/{id}", "user1"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("account not found"));
+
+    }
+
+    @Test
+    @Transactional
+    void testCreateAccount() throws Exception {
+        AccountRegisterRequest accountRequest = new AccountRegisterRequest(
+                "user1", "1234", "유저1", "user1@gmail.com");
+        mockMvc.perform(post("/api/accounts")
+                        .content(objectMapper.writeValueAsString(accountRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        assertThat(accountRepository.findAccountById("user1"))
+                .isPresent()
+                .map(AccountDto::getName)
+                .hasValue("유저1");
+    }
+
+    @Test
+    @Transactional
+    void testCreateAccountException() throws Exception {
+        AccountRegisterRequest accountRequest = new AccountRegisterRequest(
+                null, "1234", "유저", "user@gmail.com");
+        mockMvc.perform(post("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(accountRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value(
+                        "ObjectName=accountRegisterRequest,Message=must not be blank,code=NotBlank"));
+    }
+
+    @Test
+    void testModifyAccount() throws Exception {
+        AccountModifyRequest accountRequest = new AccountModifyRequest(
+                "user", "12345", "유저", "user@gmail.com", LocalDate.of(2024,1,27), "회원");
+        mockMvc.perform(put("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(accountRequest)))
+                .andExpect(status().isOk());
+        assertThat(accountRepository.findAccountById("user"))
+                .isPresent()
+                .map(AccountDto::getPassword)
+                .hasValue("12345");
+    }
+
+    @Test
+    @Transactional
+    void testModifyAccountException() throws Exception {
+        AccountModifyRequest accountRequest = new AccountModifyRequest(
+                null, "12345", "유저", "user@gmail.com", LocalDate.of(2024,1,27), "회원");
+        mockMvc.perform(put("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(accountRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value(
+                        "ObjectName=accountModifyRequest,Message=must not be blank,code=NotBlank"));
+    }
+
+    @Test
+    void testDeleteResponse() throws Exception {
+        mockMvc.perform(delete("/api/accounts/{id}", "user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString("user")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"result\":\"OK\"}"));
     }
 }
